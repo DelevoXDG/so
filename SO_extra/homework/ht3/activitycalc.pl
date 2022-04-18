1139bffcf8feff3ebcec46e294bb721460100eef
Delevo <max.on777c@gmail.com>
2022-04-18 09:32:02 +0200

    done ht3

--- SO_extra/homework/ht3/activitycalc.pl
+++ SO_extra/homework/ht3/activitycalc.pl
@@ -9,21 +9,18 @@ if (@ARGV == 0 && -t STDIN && -t STDERR) {
 	exit(-1);
 }
 
-open($FILE1, '<', @ARGV[0]) or die("Could not open file @ARGV[0]");
-open($FILE2, '<', @ARGV[0]) or die("Could not open file @ARGV[0]");
-(my $no_extension = @ARGV[0]) =~ s/\.[^.]+$//;
-open($OUT, ">", "$no_extension.html");
+open($file1, '<', @ARGV[0]) or die("Could not open file @ARGV[0]"); # To read line by line
+open($file2, '<', @ARGV[0]) or die("Could not open file @ARGV[0]"); # To slurp
+($basename = @ARGV[0]) =~ s{^.*/|\.[^.]+$}{}g; #get basename
+open($out, ">", "$basename.html");
 
+binmode $file1, ':utf8';
+binmode $file2, ':utf8';
+binmode $out, ':utf8';
+read $file2, $file_content, -s $file2;	# Slurping file in string
 
-binmode $FILE1, ':utf8';
-binmode $FILE2, ':utf8';
-binmode $OUT, ':utf8';
-binmode STDOUT, ":utf8";
-read $FILE2, $file_content, -s $FILE2;
-
-@lol=('.','.','.','.');
-
-print $OUT <<HEADER;
+# HTML boilerplate
+print $out <<HEADER;
 <html>
 <head>
 	<style>
@@ -46,86 +43,82 @@ print $OUT <<HEADER;
 </head>
 <table style="background-color:#FFF8DC">
 HEADER
-$dateregex='((?|0?[1-9]|[12][0-9]|3[01])[.](?|I(?|I{1,2}|[VX])?|VI{0,3}|X(?|I{1,3}|IV|V)?))';
-$nameregex='(\b[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż]+\s[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż]+\b)';
-@dates = ($file_content =~ /$dateregex/gi);
-@names = ($file_content =~ /$nameregex(?![\s\S]*\b\1\b)/gi);
-@names = sort @names;
 
-$cur_date;
-$cur_name;
+# Getting all the data with regex
+my $dateregex='((?|0?[1-9]|[12][0-9]|3[01])[.](?|I(?|I{1,2}|[VX])?|VI{0,3}|X(?|I{1,3}|IV|V)?))';
+my $nameregex='(\b[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż]+\s[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż]+\b)';
+my @dates = ($file_content =~ /$dateregex/gi);
+my @names = ($file_content =~ /$nameregex(?![\s\S]*\b\1\b)/gi); # Unique names
+my @names = sort @names;
 
-%students;
-%activity;
-for (@dates){
-	$activity{$_} = 0;
-}
-$activity{sum}=0;
+
+my %students;	# Hasmap (Student -> (hashmap: date->activity))
+my %activity;
+
+# for (@dates){
+# 	$activity{$_} = 0;
+# }
+# $activity{sum}=0;
 for(@names){
-
-	# $small{$_} = $activity{$_} for keys %activity;
 	my %hash_copy = %{$activity};
 	for (@dates){
 		$hash_copy{$_} = 0;
 	}
-
-	# %copy = dclone(\%activity);
+	$hash_copy{sum}= 0;
 	$students{$_} = \%hash_copy;
 }
-while (my $line = <$FILE1>) {
-	if ($line =~ /$dateregex/g){
-		($cur_date) = $1;
-	}
-	if ($line =~ /$nameregex/g){
-		($cur_name) = $1;
 
-		# print "$cur_name\n";
-		$count = () = $line =~ /[+]/g;
+# Creating empty activity hashmaps for each student
+while (my $line = <$file1>) {
+	if ($line =~ /$dateregex/g){	# Reading date
+		$cur_date = $1;
+	}
+	if ($line =~ /$nameregex/g){	# Reading name
+		$cur_name = $1;
+
+		$count = () = $line =~ /[+]/g; # Counting activity
 		$students{$cur_name}->{$cur_date} += $count;
 		$students{$cur_name}->{sum}+=$count;
 	}
 }
 
+# Student by student info stdout
+# binmode STDOUT, ":utf8";
+# foreach my $student (@names) {
+# 	# print "$student: $students{$student}\n";
+# 	print "$student:\n";
+# 	foreach my $date (@dates) {
+# 		print "$date: $students{$student}->{$date}\n";
+# 	}
+# 	print "sum: $students{$student}->{sum}\n";
+# }
 
-foreach my $student (@names) {
-
-	# print "$student: $students{$student}\n";
-	print "$student:\n";
-	foreach my $date (@dates) {
-		print "$date: $students{$student}->{$date}\n";
-	}
-	print "sum: $students{$student}->{sum}\n";
-}
-
-print $OUT "\t<tr>\n";
-print $OUT "\t\t<th style=\"text-align:left\">Imie</th>\n";
-print $OUT "\t\t<th style=\"text-align:left\">Nazwisko</th>\n";
+print $out "\t<tr>\n";
+print $out "\t\t<th style=\"text-align:left\">Imie</th>\n";
+print $out "\t\t<th style=\"text-align:left\">Nazwisko</th>\n";
 foreach my $date (@dates) {
-	print $OUT "\t\t<th style=\"text-align:left\">$date</th>\n";
+	print $out "\t\t<th style=\"text-align:left\">$date</th>\n";
 }
-print $OUT "\t\t<th style=\"text-align:left\">Suma</td>\n";
-print $OUT "\t</tr>\n";
+print $out "\t\t<th style=\"text-align:left\">Suma</td>\n";
+print $out "\t</tr>\n";
 
 foreach my $student (@names) {
-	print $OUT "\t<tr>\n";
-	my @bits = split / /, $student;
-	my $name = $bits[1];
-	my $surname = $bits[0];
-	print $OUT "\t\t<td>$name</td>\n";
-	print $OUT "\t\t<td>$surname</td>\n";
+	print $out "\t<tr>\n";
+	my @subStr = split / /, $student;
+	my $name = $subStr[1];
+	my $surname = $subStr[0];
+	print $out "\t\t<td>$name</td>\n";
+	print $out "\t\t<td>$surname</td>\n";
 	foreach my $date (@dates) {
-		print $OUT "\t\t<td>$students{$student}->{$date}</td>\n";
+		print $out "\t\t<td>$students{$student}->{$date}</td>\n";
 	}
-	print $OUT "\t\t<td>$students{$student}->{sum}</td>\n";
-	print $OUT "\t</tr>\n";
+	print $out "\t\t<td>$students{$student}->{sum}</td>\n";
+	print $out "\t</tr>\n";
 }
 
-
-print $OUT <<FOOTER;
+print $out <<FOOTER;
 </table>
 </html>
 FOOTER
-close $FILE1;
-close $OUT;
-
-# $students{'Nowak Marcelina'}->{'8.IV'} += 1;
+close $file1;
+close $out;
--- SO_extra/lab3/forloop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for I in $(seq 1 9); do
-	echo "$I"
-done
