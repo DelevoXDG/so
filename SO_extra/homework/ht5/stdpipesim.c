c39e4bee57740a774fb6c40f640c6c4476b9dbc9
Delevo <max.on777c@gmail.com>
2022-05-09 21:58:17 +0200

    old

--- SO_extra/homework/ht5/stdpipesim.c
+++ SO_extra/homework/ht5/stdpipesim.c
@@ -30,20 +30,15 @@ int makeFork() {
 	}
 	return pid;
 }
-int closePipes(int* fd, int len) {
-	for (int i = 0; i < len; i++) {
-		close(fd[i]);
-	}
-}
 
 
 int main(int argc, char* argv[]) {
 	// pthread_mutex_t zamek = PTHREAD_MUTEX_INITIALIZER;
 	char* args[][128] = {
-	{ "ls", "-l", NULL },
+	{ "ls", "-l", NULL, },
 	{ "tr","a-z" ,"A-Z", NULL },
-	// { "xargs", "echo", ">", "lol", NULL},
-	{ "cut" , "-f1", "-d", " ", NULL},
+	{ "xargs", "echo", ">", "lol", NULL},
+	// { "cut" , "-f1", "-d", " ", NULL},
 	// 	{ "ls", "-l", ".", NULL } ,
 	// 	{ "tail", "-n", "-2", NULL},
 	// { "cat", "scores", NULL},
@@ -87,48 +82,40 @@ int main(int argc, char* argv[]) {
 		}
 		default: {
 			int fd[(progCount - 1) * 2];		// Need 2 descriptors for each | occurence 
-			for (int i = 0; i < progCount - 1; i++) {
-				openPipe(fd + 2 * i);
-			}
+			openPipe(fd);
 			int pid[progCount];
-			int last = progCount - 1;
 			pid[0] = makeFork();
 			if (pid[0] == 0) {
 				dup2(fd[1], STDOUT_FILENO);		// STDOUT fd now points to pipe's write descriptor
-				// close(fd[1]);	// STDOUT already points at pipe's write fd
-				// close(fd[0]);	// Never reading input via this pipe
-				closePipes(fd, 2 * last);
+				close(fd[1]);	// STDOUT already points at pipe's write fd
+				close(fd[0]);	// Never reading input via this pipe
 				execvp(**(args + 0), *(args + 0));
 				exit(EXIT_SUCCESS);
 			}
 			for (int i = 1; i < progCount - 1; i++) {
+				// waitpid(pid[i - 1], NULL, 0);
 				pid[i] = makeFork();
 				if (pid[i] == 0) {
-					dup2(fd[0 + 2 * i], STDIN_FILENO);
-					dup2(fd[1 + 2 * i], STDOUT_FILENO);
-					// close(fd[1 + 2 * i]);		// STDOUT already points at pipe's write fd
-					// close(fd[0 + 2 * (i - 1)]);		// STDIN already points at pipe's read fd
-					closePipes(fd, 2 * last);
+					dup2(fd[1], STDOUT_FILENO);
+					dup2(fd[0], STDIN_FILENO);
+					close(fd[1]);		// STDOUT already points at pipe's write fd
+					close(fd[0]);		// STDIN already points at pipe's read fd
 					execvp(**(args + i), *(args + i));
 					// execlp("tail", "tail", "-n", "-2", NULL);
 					exit(EXIT_SUCCESS);
 				}
 			}
-
-			pid[last] = makeFork();
-			if (pid[last] == 0) {
-				dup2(fd[0 + (last - 1) * 2], STDIN_FILENO);
-				// close(fd[0 + (last - 1) * 2]);
-				// close(fd[1 + (last - 1) * 2]);
-				closePipes(fd, 2 * last);
-				execvp(**(args + last), *(args + last));
+			pid[progCount - 1] = makeFork();
+			if (pid[progCount - 1] == 0) {
+				dup2(fd[0], STDIN_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				execvp(**(args + progCount - 1), *(args + progCount - 1));
 				// execlp("tail", "tail", "-n", "-2", NULL);
 				exit(EXIT_SUCCESS);
 			}
-			// for (int i = 0; i < 2 * last; i++) {
-			// 	close(fd[i]);
-			// }
-			closePipes(fd, 2 * last);
+			close(fd[0]);
+			close(fd[1]);
 			for (int i = 0; i < progCount; i++) {
 				waitpid(pid[i], NULL, 0);
 			}
