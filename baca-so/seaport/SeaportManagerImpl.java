82cc5b6c1e3069703594ee6e1cddd7d2a2ccc6ba
Delevo <max.on777c@gmail.com>
2022-06-12 20:10:54 +0200

    86%

--- baca-so/seaport/SeaportManagerImpl.java
+++ baca-so/seaport/SeaportManagerImpl.java
@@ -1,35 +1,7 @@
-
 // Maksim Zdobnikau
-
-// Napisz klasę implementującą interfejs SeaportManager o nazwie SeaportManagerImpl, która będzie właściwie zarządzała ruchem w porcie. Osiągnij to poprzez
-// odpowiednie blokowanie wątku w metodach z interfejsu SeaportManager. Każdy statek zaimplementowany jest jako wątek, który wywołuje kolejno metody:
-
-// requestSeawayEntrance() - wywoływana przy zbliżaniu się do kanału; statek zacznie wpływać do kanału po powrocie z tej metody.
-// requestPortEntrance() - wywoływana przy zbliżaniu się do portu; statek zacznie wpływać do portu po powrocie z tej metody; liczba całkowita zwrócona przez tą
-// metodę określa najniższy numer doku spośród doków, do których statek zacumuje.
-// signalPortEntered() - wywoływana po opuszczeniu kanału (podczas wpływania do portu).
-// requestPortExit() - wywoływana przed opuszczeniem portu; statek zacznie wpływać do kanału po powrocie z tej metody.
-// signalPortExited() - wywoływana po opuszczeniu portu.
-// signalShipSailedAway() - wywoływana tuż po opuszczeniu kanału (podczas wypływania na otwarte morze).
-// Twój program musi spełnić następujące warunki:
-
-// W kanale może się znaleźć maksymalnie M statków jednocześnie.
-// Dany dok może być zajmowany przez maksymalnie jeden statek jednocześnie.
-// Numer najniższego doku, do którego statek ma zacumować, musi zawierać się w poprawnym zakresie.
-// Zasoby muszą być wykorzystywane optymalnie (równoległość implementacji).
-// Każdy statek musi zakończyć swoją podróż (brak zakleszczeń).
-// Dodatkowe informacje:
-
-// Jako pierwsza zostanie wywołana metoda init() poprzez którą Twoja klasa otrzyma parametry portu (liczba doków, pojemność kanału).
-// Metoda requestPortEntrance() zwraca numer wyznaczający doki, do których statek zacumuje. Jest to najniższy numer doku spośród doków, do których statek
-// zacumuje. Doki są numerowane od zera. Przykładowo, jeśli rozmiar statku wynosi k = 3 i metoda zwróci liczbę 1, to statek zacumuje do doków numer 1, 2 oraz 3.
-// Statek będzie pamiętał ten numer po przypisaniu i możesz go sprawdzić metodą getAssignedDock() interfejsu Ship.
-// Rozmiar statku sprawdzisz metodą getDockingSize() interfejsu Ship.
-// Nie ma żadnych innych gwarancji co do wątków poza kolejnością wywołań metod - każdy wątek może dowolnie dobierać czas pomiędzy wspomnianymi wywołaniami.
 
 import java.util.concurrent.*;
 import java.util.concurrent.locks.*;
-import java.util.concurrent.atomic.*;
 
 public class SeaportManagerImpl implements SeaportManager {
 	int docksCount;
@@ -37,63 +9,129 @@ public class SeaportManagerImpl implements SeaportManager {
 	int seawayCount;
 	int seawayPut;
 	int seawayTake;
-	boolean[] docksTaken;
-	Semaphore entranceLock = new Semaphore(1);
-	Lock seawayLock = new ReentrantLock();
-	final Condition seawayNotFull = seawayLock.newCondition();
-	final Condition seawayNotEmpty = seawayLock.newCondition();
+	boolean[] reserved;
+	Semaphore canalPermits;
+	Semaphore docksPermits;
+	Semaphore docksBooked;
+	Semaphore arrSem = new Semaphore(1);
+	// Lock seawayLock = new ReentrantLock();
+	// final Condition seawayNotFull = seawayLock.newCondition();
+	// final Condition seawayNotEmpty = seawayLock.newCondition();
 
 	@Override public void init(int numberOfDocks, int seawayCapacity) {
 		this.docksCount = numberOfDocks;
 		this.seawayCapacity = seawayCapacity;
 		this.seawayCount = 0;
-		this.docksTaken = new boolean[numberOfDocks];
-	}
-
-	@Override public void requestSeawayEntrance(Ship s) {
-		seawayLock.lock();
-		try {
-			while (seawayCount == seawayCapacity) {
-				try {
-					seawayNotFull.await();
-				} catch (InterruptedException e) {
-					return;
-				}
-				if (++seawayPut == seawayCount) {
-					seawayPut = 0;
-				}
-				seawayCount++;
-				seawayNotEmpty.signal();
-			}
-		} finally {
-			seawayLock.unlock();
+		this.canalPermits = new Semaphore(seawayCapacity);
+		this.docksPermits = new Semaphore(docksCount);
+		this.docksBooked = new Semaphore(docksCount);
+		this.reserved = new boolean[numberOfDocks];
+		for (int i = 0; i < numberOfDocks; i++) {
+			this.reserved[i] = false;
 		}
 	}
 
-	@Override public int requestPortEntrance(Ship s) {
+	@Override public void requestSeawayEntrance(Ship s) {
+		semAcquire(docksBooked, s.getDockingSize());
+		semAcquire(canalPermits, 1);
 
-		// TODO Auto-generated method stub
-		return 0;
+		// seawayLock.lock();
+		// synchronized (this) {
+		// 	try {
+		// 		while (s.getDockingSize() > bookedDocks.availablePermits()) {
+		// 			semRelease(entranceLock);
+		// 			try {
+		// 				seawayNotFull.await();
+		// 			} catch (InterruptedException e) {
+		// 				return;
+		// 			}
+		// 			semAcquire(entranceLock);
+		// 		}
+		// 	} finally {
+		// 		seawayLock.unlock();
+		// 	}
+		// }
+	}
+
+	@Override public int requestPortEntrance(Ship s) {
+		semAcquire(docksPermits, s.getDockingSize());
+		semAcquire(arrSem, 1);
+		int place = this.reserve(s.getDockingSize());
+		semRelease(arrSem, 1);
+
+		return place;
 	}
 
 	@Override public void signalPortEntered(Ship s) {
-		// TODO Auto-generated method stub
-
+		semRelease(canalPermits, 1);
 	}
 
 	@Override public void requestPortExit(Ship s) {
-		// TODO Auto-generated method stub
-
+		semAcquire(canalPermits, 1);
+		semRelease(docksBooked, s.getDockingSize());
 	}
 
 	@Override public void signalPortExited(Ship s) {
-		// TODO Auto-generated method stub
-
+		semRelease(docksPermits, s.getDockingSize());
+		semAcquire(arrSem, 1);
+		this.unReserve(s.getAssignedDock(), s.getDockingSize());
+		semRelease(arrSem, 1);
 	}
 
 	@Override public void signalShipSailedAway(Ship s) {
-		// TODO Auto-generated method stub
-
+		semRelease(canalPermits, 1);
+	}
+	private void semAcquire(Semaphore sem) { // Helper function to post on semaphore
+		try {
+			sem.acquire();
+			// System.out.println("Aquired : " + sem.toString());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	private void semAcquire(Semaphore sem, int count) { // Helper function to post on semaphore
+		try {
+			sem.acquire(count);
+			// System.out.println("Aquired : " + sem.toString());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
+	private void semRelease(Semaphore sem) { // Helper function to release semaphore
+		try {
+			// System.out.println("Released : " + sem.toString());
+			sem.release();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	private void semRelease(Semaphore sem, int count) { // Helper function to release semaphore
+		try {
+			// System.out.println("Released : " + sem.toString());
+			sem.release(count);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	private synchronized int reserve(int size) {
+		int	start	= 0;
+		int	j		= 0;
+		for (int i = 0; i < docksCount && j < size; i++) {
+			if (reserved[i] == false) {
+				start = i;
+				while (j < size) {
+					reserved[start + j] = true;
+					j++;
+				}
+			}
+		}
+		return start;
+	}
+	private synchronized void unReserve(int start, int size) {
+		for (int i = start; i < start + size; i++) {
+			reserved[i] = false;
+		}
+
+	}
 }
